b1323b44cfa0d2d20ef398e1cd22af9ca243aaac
shifumix <38515045+shifumix@users.noreply.github.com>
2018-09-07 12:45:19 +0200

    Create rpi_to_kiosk.sh

---
+++ rpi_to_kiosk.sh
@@ -0,0 +1,21 @@
+#Script de preparation de l'image pour rpi en mode kiosque
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+
+sudo apt-get install chromium-browser unclutter lxde
+sudo iwlist wlan0 scan
+
+sudo echo "network={ssid='RVNetworkMobile' psk='42714271'}" >> "/etc/wpa_supplicant/wpa_supplicant.conf"
+
+sudo echo "@xset s off" >> ~/.config/lxsession/LXDE/autostart
+sudo echo "@xset -dpms" >> ~/.config/lxsession/LXDE/autostart
+sudo echo "@xset s noblank" >> ~/.config/lxsession/LXDE/autostart
+sudo echo "@sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium-browser Default/Preferences" >> ~/.config/lxsession/LXDE/autostart
+sudo echo "@chromium-browser --noerrdialogs --kiosk https://blockdev.io --incognito --disable-translate" >> ~/.config/lxsession/LXDE/autostart
+
+sudo echo "@lxpanel --profile LXDE-pi" >> ~/.config/lxsession/LXDE/autostart
+sudo echo "@pcmanfm --desktop --profile LXDE-pi" >> ~/.config/lxsession/LXDE/autostart
+
+sudo reboot
